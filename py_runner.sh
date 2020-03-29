40318386e0aab39ee0232b752add3b3aeb794e7b
laura chan <laura.chan.2010@gmail.com>
2020-03-29 18:11:07 -0400

    use regular expression

--- py_runner.sh
+++ py_runner.sh
@@ -9,12 +9,12 @@ fi
 
 ${THIS_DIR}/bin/hadoop dfsadmin -safemode leave
 ${THIS_DIR}/bin/hadoop fs -mkdir -p ${THIS_DIR}/py/sample
-${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}/sample/* ${THIS_DIR}/py/sample
+${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}sample/* ${THIS_DIR}/py/sample
 ${THIS_DIR}/bin/hadoop jar \
     ${THIS_DIR}/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
-    -file ${THIS_DIR}/py/${SUB_DIR}/mapper.py \
-    -mapper ${THIS_DIR}/py/${SUB_DIR}/mapper.py \
-    -file ${THIS_DIR}/py/${SUB_DIR}/reducer.py	\
+    -file ${THIS_DIR}/py/${SUB_DIR}mapper.py \
+    -mapper ${THIS_DIR}/py/${SUB_DIR}mapper.py \
+    -file ${THIS_DIR}/py/${SUB_DIR}reducer.py	\
     -reducer "${THIS_DIR}/py/${SUB_DIR}/reducer.py ${*:2}" \
     -input ${THIS_DIR}/py/sample/* \
     -output ${THIS_DIR}/py-output
--- py_runner2.sh
+++ py_runner2.sh
@@ -8,19 +8,19 @@ then SUB_DIR="$1/"
 fi
 
 if [ -n "$2" ];
-then PATTERN="$2/" 
+then PATTERN="$2" 
 fi
 
 
 ${THIS_DIR}/bin/hadoop dfsadmin -safemode leave
 ${THIS_DIR}/bin/hadoop fs -mkdir -p ${THIS_DIR}/py/sample
-${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}/sample/* ${THIS_DIR}/py/sample
+${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}sample/* ${THIS_DIR}/py/sample
 ${THIS_DIR}/bin/hadoop jar \
     ${THIS_DIR}/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
-    -file ${THIS_DIR}/py/${SUB_DIR}/mapper.py \
-    -mapper '${THIS_DIR}/py/${SUB_DIR}/mapper.py $PATTERN'\
-    -file ${THIS_DIR}/py/${SUB_DIR}/reducer.py	\
-    -reducer "${THIS_DIR}/py/${SUB_DIR}/reducer.py ${*:2}" \
+    -file ${THIS_DIR}/py/${SUB_DIR}mapper.py \
+    -mapper '${THIS_DIR}/py/${SUB_DIR}mapper.py $PATTERN'\
+    -file ${THIS_DIR}/py/${SUB_DIR}reducer.py	\
+    -reducer "${THIS_DIR}/py/${SUB_DIR}reducer.py ${*:2}" \
     -input ${THIS_DIR}/py/sample/* \
     -output ${THIS_DIR}/py-output
 ${THIS_DIR}/bin/hdfs dfs -cat ${THIS_DIR}/py-output/*
