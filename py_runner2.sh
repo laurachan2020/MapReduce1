45dcdb0290bc282b98cbf1675cfeb50ca5205c89
laura chan <laura.chan.2010@gmail.com>
2020-03-29 17:18:04 -0400

    py_runner2.py handle mapper.py input parameters

---
+++ py_runner2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/etc/bootstrap.sh
+THIS_DIR=/usr/local/hadoop
+
+if [ -n "$1" ];
+then SUB_DIR="$1/" 
+fi
+
+if [ -n "$2" ];
+then GREP_WORD="$2/" 
+fi
+
+
+${THIS_DIR}/bin/hadoop dfsadmin -safemode leave
+${THIS_DIR}/bin/hadoop fs -mkdir -p ${THIS_DIR}/py/sample
+${THIS_DIR}/bin/hdfs dfs -put ${THIS_DIR}/py/${SUB_DIR}/sample/* ${THIS_DIR}/py/sample
+${THIS_DIR}/bin/hadoop jar \
+    ${THIS_DIR}/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar \
+    -file ${THIS_DIR}/py/${SUB_DIR}/mapper.py \
+    -mapper ${THIS_DIR}/py/${SUB_DIR}/mapper.py $GREP_WORD\
+    -file ${THIS_DIR}/py/${SUB_DIR}/reducer.py	\
+    -reducer "${THIS_DIR}/py/${SUB_DIR}/reducer.py ${*:2}" \
+    -input ${THIS_DIR}/py/sample/* \
+    -output ${THIS_DIR}/py-output
+${THIS_DIR}/bin/hdfs dfs -cat ${THIS_DIR}/py-output/*
