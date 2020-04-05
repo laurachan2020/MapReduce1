1f61152c7b0f6ef4a6e149b5fc72050d849242cd
laura chan <laura.chan.2010@gmail.com>
2020-04-05 16:43:58 -0400

    remove first charactor

--- webaccesslog/mapper2.py
+++ webaccesslog/mapper2.py
@@ -10,16 +10,12 @@ for line in sys.stdin:
     line = line.strip()
     # split the line into words
     words = line.split(',')
-    try:
-        if len(words)==4:
-            test = pat.match(words[0])
-            if test:
-                if words[2].startswith('GET')  or words[2].startswith('PUT') or words[2].startswith('POST') or words[2].startswith('DELETE'):
-                    elements = words[2].split()
-                    if '.php' in elements[1]:
-                        index = elements[1].index('.php')
-                        pagename = elements[1].[:index+4]
-                        print '%s\t%s' % (pagename, 1)
-    except:
-        pass
+    if len(words)==4:
+        test = pat.match(words[0])
+        if test:
+            elements = words[2].split()
+            if '.php' in elements[1]:
+                index = elements[1].index('.php')
+                pagename = elements[1][1:index+4]
+                print '%s\t%s' % (pagename, 1)
 
