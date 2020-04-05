de93cc0dc008bbca2de10b7c75a13438785e6d68
laura chan <laura.chan.2010@gmail.com>
2020-04-05 14:57:39 -0400

    add code for webaccesslog

---
+++ webaccesslog/mapper1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+import re
+pattern = sys.argv[1]
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        if re.match(pattern, word):
+            print '%s\t%s' % (word, 1)
+        else:
+            print '%s\t%s' % (word, 0)
