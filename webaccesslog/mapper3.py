3dc72b1f4339d1e971709a2624f373967bffeca8
laura chan <laura.chan.2010@gmail.com>
2020-04-05 21:42:25 -0400

    add code to calculate many accesses occurred before noon vs afternoon

---
+++ webaccesslog/mapper3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""mapper2.py"""
+import sys
+import re
+import time
+pat = re.compile("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
+datetime_pattern = '%d/%b/%Y:%H:%M:%S'
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split(',')
+    if len(words)==4:
+        test = pat.match(words[0])
+        if test:
+            log_time = time.strptime(words[1][1:],datetime_pattern)
+            if log_time.tm_hour < 12:
+                print '%s\t%s' % ('before noon', 1)
+            else
+                print '%s\t%s' % ('afternoon', 1)
